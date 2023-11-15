4be8ae4a9c154d3a45bdde3dbb8b55d2f1f3e4f8
Roland Zhao <roland.zhao@mail.utoronto.ca>
2023-11-15 16:52:29 -0500

    blog project initial backend

---
+++ part4/app.js
@@ -0,0 +1,33 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const morgan = require('morgan')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.info('error connecting to MongoDB: ', error.message)
+  })
+
+morgan.token('data', (request) => JSON.stringify(request.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
---
+++ part4/models/blog.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  author: {
+    type: String,
+    minLength: 1,
+    required: true
+  },
+  url: {
+    type: String,
+    validate: {
+      validator: (v) => {
+        return /^(ftp|http|https):\/\/[^ "]+$/.test(v)
+      },
+      message: '{VALUE} is not a valid link'
+    },
+    required: true
+  },
+  likes: {
+    type: Number,
+    default: 0
+  }
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
