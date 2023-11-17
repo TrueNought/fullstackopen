b82bba247dbf6825d693b4212281d2522e5df3f6
Roland Zhao <roland.zhao@mail.utoronto.ca>
2023-11-17 13:46:39 -0500

    unit tests up to 4.5

---
+++ part4/utils/list_helper.js
@@ -0,0 +1,27 @@
+const totalLikes = (blogs) => {
+  return blogs.reduce((total, blog) => {
+    return total + blog.likes
+  }, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return {}
+  }
+  const favorite = blogs.reduce((most, blog) => {
+    return blog.likes > most.likes
+      ? blog
+      : most
+  }, blogs[0])
+
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes,
+  }
+}
+
+module.exports =  {
+  totalLikes,
+  favoriteBlog
+}
