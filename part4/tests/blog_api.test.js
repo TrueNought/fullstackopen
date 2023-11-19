d5dba188a10aa9c0da68ed24ee958a20eb2fb9d3
Roland Zhao <roland.zhao@mail.utoronto.ca>
2023-11-18 21:03:50 -0500

    exercise 4.8

---
+++ part4/tests/blog_api.test.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: 'Test Blog',
+    author: 'Tester 1',
+    url: 'https://test1.com',
+    likes: 33
+  },
+  {
+    title: 'Another Test',
+    author: 'Tester 2',
+    url: 'https://test2.com',
+    likes: 20
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('correct number of blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(initialBlogs.length)
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
---
+++ part4/tests/teardown.js
@@ -0,0 +1,5 @@
+// temp fix for Mongoose 6.x error with Jest
+
+module.exports = () => {
+  process.exit(1)
+}
