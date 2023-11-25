ced9bcb6b8f650335e9dacd0aba3f6aec915c7e8
Roland Zhao <roland.zhao@mail.utoronto.ca>
2023-11-25 14:08:57 -0500

    moved token extraction to middleware

--- part4/app.js
+++ part4/app.js
@@ -27,6 +27,7 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :d
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
