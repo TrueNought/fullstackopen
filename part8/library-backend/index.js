72920283e1bc012bf9c9ed47cec7c896bca402e2
Roland Zhao <roland.zhao@mail.utoronto.ca>
2024-02-05 19:44:47 -0500

    8.13-8.15 store data in MongoDB

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,88 +1,24 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
-const { v1: uuid } = require('uuid')
+const { GraphQLError } = require('graphql')
 
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821
-  },
-  { 
-    name: 'Joshua Kerievsky',
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  { 
-    name: 'Sandi Metz',
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-]
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
+const Book = require('./models/book')
+const Author = require('./models/author')
+require('dotenv').config()
 
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },  
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'The Demon',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
-  },
-]
+const MONGODB_URI = process.env.MONGODB_URI
+console.log('connecting to', MONGODB_URI)
 
-/*
-  you can remove the placeholder query once your first one has been implemented 
-*/
+mongoose.connect(MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB: ', error.message)
+  })
+
 
 const typeDefs = `
   type Query {
@@ -102,7 +38,7 @@ const typeDefs = `
   type Book {
     title: String!
     published: Int!
-    author: String!
+    author: Author!
     genres: [String!]!
     id: ID!
   }
@@ -124,47 +60,72 @@ const typeDefs = `
 
 const resolvers = {
   Query: {
-    bookCount: () => books.length,
-    authorCount: () => authors.length,
-    allBooks: (root, args) => {
+    bookCount: async () => Book.collection.countDocuments(),
+    authorCount: async () => Author.collection.countDocuments(),
+    allBooks: async (root, args) => {
       if (!args) {
-        return books
+        return Book.find({}).populate('author')
       }
-      let result = books
+      let query = {}
       if (args.author) {
-        result = result.filter(book => book.author === args.author)
+        const author = await Author.findOne({ name: args.author })
+        if (!author) {
+          return []
+        }
+        query.author = author._id
       }
       if (args.genre) {
-        result = result.filter(book => book.genres.includes(args.genre))
+        query.genres = args.genre
       }
-      return result
+      return Book.find(query).populate('author')
     },
-    allAuthors: () => authors
+    allAuthors: async () => Author.find({})
   },
   
   Author: {
-    bookCount: (root) => {
-      return books.filter(book => book.author === root.name).length
+    bookCount: async (root) => {
+      let author = await Author.findOne({ name: root.name })
+      if (!author) {
+        return 0
+      }
+      return Book.countDocuments({ author: author._id })
     }
   },
 
   Mutation: {
-    addBook: (root, args) => {
-      const newBook = { ...args, id: uuid() }
-      if (!authors.some(author => author.name === args.author)) {
-        const newAuthor = { name: args.author, id: uuid() }
-        authors = authors.concat(newAuthor)
+    addBook: async (root, args) => {
+      if (args.title.length < 5) {
+        throw new GraphQLError('Title must be at least 5 characters long', {
+          extensions: {
+            code: 'GRAPHQL_VALIDATION_FAILED',
+            invalidArgs: args.title 
+          }
+        })
       }
-      books = books.concat(newBook)
+      let author = await Author.findOne({ name: args.author })
+      if (!author) {
+        if (args.author.length < 5) {
+          throw new GraphQLError('Author must be at least 4 characters long', {
+            extensions: {
+              code: 'GRAPHQL_VALIDATION_FAILED',
+              invalidArgs: args.author
+            }
+          })
+        }
+        author = new Author({ name: args.author })
+        await author.save()
+      }
+      const newBook = new Book({ ...args, author: author })
+      await newBook.save()
       return newBook
     },
-    editAuthor: (root, args) => {
-      const author = authors.find(a => a.name === args.name)
-      if (!author) {
-        return null
-      }
-      const updatedAuthor = { ...author, born: args.setBornTo }
-      authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+
+    editAuthor: async (root, args) => {
+      const updatedAuthor = await Author.findOneAndUpdate(
+        { name: args.name },
+        { born: args.setBornTo },
+        { new: true }
+      )
       return updatedAuthor
     }
   }
